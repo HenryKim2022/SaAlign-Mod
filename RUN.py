90dd22df6588b7866ea6e2241c1d4c828d25c7d2
HenryKim2022 <autromatron@gmail.com>
2023-10-10 21:44:09 +0700

    updated RUN.py

--- RUN.py
+++ RUN.py
@@ -3,11 +3,10 @@ import os
 
 current_script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# memory = "512m"   #512MB RAM
-memory = "1g"  # 1GB RAM
-application_name = "SaAlign_MultipleDNAorRNA"  #
-filename = "./dataset/covid_10.fasta/"
-basepath = current_script_directory
+memory = "1g"  # 1g or 512m
+application_name = "SaAlign_MultipleDNAorRNA"  # App Name
+filename = "./dataset/covid_10.fasta/"  # Data Set
+basepath = current_script_directory     # Current Directory
 
 sa_align(memory, application_name, filename,
          basepath, partition=2, url="local[*]")
